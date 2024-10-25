4bd350bdc15941f680f0d8b515196b318ebfbcf5
jivanrod <jivanrod@gmail.com>
2024-10-25 17:27:49 +0200

    fix

--- index.php
+++ index.php
@@ -1,38 +1,82 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>Pogramación con librería Datetime</title>
         <meta name="viewport" content="width=device-width">
         <meta charset="UTF-8">
-        <title>Uso DateTime</title>
-        <style>
-            p {
-                font-weight: bold;
-            }
-        </style>
+        <link rel="stylesheet" href="stylesheet.css">
     </head>
     <body>
-        <p> Crea una instancia de la clase DateTime para la fecha actual </p>
-
-        <p> Cambia el huso horario de la fecha actual a la zona de 'America/Los_Angeles' </p>
-
-        <p> Muestra la fecha actual en un formato específico Año-mes-dia Hora:minuto:segundo </p>
-
-        <p> Modifica y muestra la fecha según el formato W3C que corresponde a la fecha de hace una semana </p>
-
-        <p> Crea una instancia de la clase DateTime para una fecha específica a partir de la cadena "20/12/2024". Muestra el objeto DateTime </p>
-
-        <p> Muestra la fecha específica en el formato "20 December 2024"</p>
-
-        <p> Muestra el timestamp de dicha fecha </p>
-
-        <p> Calcula la diferencia en días entre las fecha actual y la fecha específica </p>
-
-        <p> Suma un intervalo de tiempo de dos meses a la fecha específica </p>
-
-        <p> Resta un intervalo de tiempo de 5 días a la fecha específica </p>
-
-        <p> Compara la fechas actual y la específica para saber cuál es anterior </p> 
-
-        <p> Muestra las fechas que hay entre la fecha actual y la específica con un intervalo de una semana </p>
+        <h2>Programación con librería Datetime</h2>
+        <table>
+            <tr><td><p>Crea una instancia de la clase DateTime para la fecha actual</p></td></tr>
+            <tr>
+                <td><!-- Escribe tu código aquí --></td>
+            </tr>
+            <tr><td><p>Cambia el huso horario de la fecha actual a la zona de 'America/Los_Angeles'</p></td></tr>
+            <tr>
+                <td><!-- Escribe tu código aquí --></td>
+            </tr>
+            <tr>
+                <td><p>Muestra la fecha actual en un formato específico Año-mes-dia Hora:minuto:segundo</p></td>
+            </tr>
+            <tr>
+                <td><!-- Escribe tu código aquí --></td>
+            </tr>
+            <tr>
+                <td><p>Modifica y muestra la fecha según el formato W3C que corresponde a la fecha de hace una semana</p></td>
+            </tr>
+            <tr>
+                <td><!-- Escribe tu código aquí --></td>
+            </tr>
+            <tr>
+                <td><p>Crea una instancia de la clase DateTime para una fecha específica a partir de la cadena "20/12/2024". Muestra el objeto DateTime</p></td>
+            </tr>
+            <tr>
+                <td><!-- Escribe tu código aquí --></td>
+            </tr>
+            <tr>
+                <td><p>Muestra la fecha específica en el formato "20 December 2024"</p></td>
+            </tr>
+            <tr>
+                <td><!-- Escribe tu código aquí --></td>
+            </tr>
+            <tr>
+                <td><p>Muestra el timestamp de dicha fecha</p></td>
+            </tr>
+            <tr>
+                <td><!-- Escribe tu código aquí --></td>
+            </tr>
+            <tr>
+                <td><p>Calcula la diferencia en días entre las fecha actual y la fecha específica</p></td>
+            </tr>
+            <tr>
+                <td><!-- Escribe tu código aquí --></td>
+            </tr>
+            <tr>
+                <td><p>Suma un intervalo de tiempo de dos meses a la fecha específica</p></td>
+            </tr>
+            <tr>
+                <td><!-- Escribe tu código aquí --></td>
+            </tr>
+            <tr>
+                <td><p>Resta un intervalo de tiempo de 5 días a la fecha específica</p></td>
+            </tr>
+            <tr>
+                <td><!-- Escribe tu código aquí --></td>
+            </tr>
+            <tr>
+                <td><p>Compara la fechas actual y la específica para saber cuál es anterior</p></td>
+            </tr>
+            <tr>
+                <td><!-- Escribe tu código aquí --></td>
+            </tr>
+            <tr>
+                <td><p>Muestra las fechas que hay entre la fecha actual y la específica con un intervalo de una semana</p></td>
+            </tr>
+            <tr>
+                <td><!-- Escribe tu código aquí --></td>
+            </tr>
+        </table>
     </body>
 </html>
